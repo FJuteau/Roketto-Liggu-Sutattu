1055a442e8c9539a976eaadb6d441bd111fff98a
François Juteau <fjuteau@vente-privee.com>
2018-01-07 16:19:43 +0100

    initial commit

---
+++ Roketto Liggu Sutattu/Network/Datamanager.swift
@@ -0,0 +1,17 @@
+//
+//  Datamanager.swift
+//  Roketto Liggu Sutattu
+//
+//  Created by François Juteau on 07/01/2018.
+//  Copyright © 2018 Momo. All rights reserved.
+//
+
+import Foundation
+
+class Datamanager {
+    
+    static let apiURL
+    class func request() {
+        
+    }
+}
---
+++ Roketto Liggu Sutattu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roketto Liggu Sutattu
+//
+//  Created by François Juteau on 07/01/2018.
+//  Copyright © 2018 Momo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
