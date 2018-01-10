52e499696b5e7c8293e21ff4e477b1feba4073a3
Juteau François <f.juteau@gmail.com>
2018-01-10 20:58:11 +0100

    v2

---
+++ Roketto Liggu Sutattu/Search/Playlist/Cell/PlaylistTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlaylistTableViewCell.swift
+//  Roketto Liggu Sutattu
+//
+//  Created by François Juteau on 07/01/2018.
+//  Copyright © 2018 Momo. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
