a640a4f08dc49aef242d5331fe14944b17e86555
François Juteau <fjuteau@vente-privee.com>
2018-01-10 21:45:10 +0100

    model

---
+++ Roketto Liggu Sutattu/Network/Model/Playlist.swift
@@ -0,0 +1,22 @@
+//
+//  Playlist.swift
+//  Roketto Liggu Sutattu
+//
+//  Created by François Juteau on 10/01/2018.
+//  Copyright © 2018 Momo. All rights reserved.
+//
+
+import Foundation
+
+struct Playlist {
+    let rankType: String
+    let rankLeagueName: String
+    let rankLeague: Int
+    let rankDivision: Int
+    
+    let rating: Int
+    let topRating: Int
+    
+    let rankImageUrlString: String
+    
+}
